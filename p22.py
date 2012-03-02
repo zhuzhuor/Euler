4be1e60e2c4f5dbeebd4583c20c346b88f21255e
Bo Zhu <zhubo03@gmail.com>
2012-03-02 14:20:30 -0500

    p22

---
+++ p22.py
@@ -0,0 +1,15 @@
+with open('names.txt') as f:
+    l = eval('[' + f.read() + ']')
+l.sort()
+
+def score(s):
+    r = 0
+    for c in s:
+        r += ord(c) - ord('A') + 1
+    return r
+
+sum = 0
+for i in range(len(l)):
+    sum += score(l[i]) * (i + 1)
+print sum
+
