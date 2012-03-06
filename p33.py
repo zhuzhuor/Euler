5cb525e4803e5d2117fa3cde90866a427053e112
Bo Zhu <zhubo03@gmail.com>
2012-03-05 21:13:00 -0500

    understanding p33 is a big problem...

---
+++ p33.py
@@ -0,0 +1,24 @@
+def cancel(a, b):
+    """cancel the common terms of a and b"""
+    al, ar = "%02d" % a
+    bl, br = "%02d" % b
+    if al == br:
+        al = ''
+        br = ''
+    if ar == bl:
+        ar = ''
+        bl = ''
+    a = al + ar
+    b = bl + br
+    if a == '' or b == '' or a == '0' or b == '0':
+        return None
+    elif len(a) == 2:
+        return None
+    else:
+        return float(a) / float(b)
+
+for a in range(1, 100):
+    for b in range(a + 1, 100):
+        ret = cancel(a, b)
+        if ret != None and ret == float(a) / float(b):
+            print a, b
